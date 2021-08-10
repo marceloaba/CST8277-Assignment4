6e87cb62334341b0e17d62d9c32808c1a96756c9
mich0216 <mich0216@algonquinlive.com>
2021-08-10 12:37:41 -0400

    chrish commite

---
+++ REST-BloodBank-Skeleton/src/main/java/bloodbank/rest/resource/PhoneResource.java
@@ -0,0 +1,81 @@
+package bloodbank.rest.resource;
+
+import static bloodbank.utility.MyConstants.ADMIN_ROLE;
+import static bloodbank.utility.MyConstants.USER_ROLE;
+
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import bloodbank.ejb.BloodBankService;
+import bloodbank.entity.Phone;
+
+@Path("phone")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PhoneResource {
+
+	private static final Logger LOG = LogManager.getLogger();
+
+	@EJB
+	protected BloodBankService service;
+
+	@Inject
+	protected SecurityContext sc;
+	
+	@GET
+    @RolesAllowed({ADMIN_ROLE})
+	public Response getPhones() {
+		LOG.debug("retrieving all phones ...");
+		List<Phone> phones = service.getAll(Phone.class, "Phone.findAll");
+		LOG.debug("Phones found = {}", phones);
+		Response response = Response.ok(phones).build();
+		return response;
+	}
+	
+	@GET
+	@RolesAllowed({ADMIN_ROLE, USER_ROLE})
+	@Path("/{phoneID}")
+	public Response getPhoneById(@PathParam("phoneID") int phoneId) {
+		LOG.debug("Retrieving phone with id = {}", phoneId);
+		Phone phone = service.getById(Phone.class, "Phone.findAll", phoneId);
+		Response response = Response.ok(phone).build();
+		return response;
+	}
+	
+	@RolesAllowed({ADMIN_ROLE})
+	@POST
+	public Response addPhone(Phone newPhone) {
+		LOG.debug("Adding a new phone = {}", newPhone);
+		Phone tempPhone = service.persistPhone(newPhone);
+		return Response.ok( tempPhone).build();
+	}
+	
+	@DELETE
+	@RolesAllowed({ADMIN_ROLE})
+	@Path("/{phoneID}")
+	public Response deleteAddress(@PathParam("phoneID") int phoneID) {
+		LOG.debug("Deleting a specific phone with id = {}", phoneID);
+		Phone phone = service.getById(Phone.class, Phone.PHONES_QUERY_BY_ID, phoneID);
+		service.deletePhoneById(phoneID);
+		Response response = Response.ok(phone).build();
+		return response;
+	}
+	
+}
+
